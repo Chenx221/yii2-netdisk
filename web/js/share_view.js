95ef6938814efcfffaf8eabcef3413d406c7c09c
Chenx221 <chenx221@yandex.com>
2024-02-17 16:53:11 +0800

    分享链接复制功能改进 现在复制的链接自带访问密码

--- web/js/share_view.js
+++ web/js/share_view.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     $('#copy-link-button').click(function() {
         var shareId = $('table.detail-view tbody tr:first-child td').text();
-        var shareLink = window.location.origin + '/index.php?r=share%2Faccess&share_id=' + shareId;
+        var accessCode = $('table.detail-view tbody tr:nth-child(3) td').text();  // 获取访问密码
+        var shareLink = window.location.origin + '/index.php?r=share%2Faccess&share_id=' + shareId + '&access_code=' + accessCode;  // 将访问密码添加到分享链接中
         navigator.clipboard.writeText(shareLink).then(function() {
             alert('分享链接已复制到剪贴板');
         }).catch(function(error) {
